7783b48743a29a40a522a0b306ca54692dc826ca
Vladimir2911 <sidak29.11@gmail.com>
2025-06-28 18:20:13 +0300

    Добавьте файлы проекта.

---
+++ ToDoList/Controllers/TaskController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTasks(int count)
+        {
+            List<ToDoTask> tasks = new List<ToDoTask>();
+
+            for (int i = 0; i < count; i++)
+            {
+                tasks.Add(new ToDoTask() { Name = $"Name+ {i}" });
+            }
+            return Ok(tasks);
+        }
+    }
+}
---
+++ ToDoList/Models/ToDoTask.cs
@@ -0,0 +1,7 @@
+﻿namespace ToDoList.Models
+{
+    public class ToDoTask
+    {
+        public string Name { get; set; }
+    }
+}
